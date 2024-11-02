4152f2b83a02d1e5b40c2220c1b4483267d501c2
hardikkum444 <hardikkumawat444@gmail.com>
2024-11-02 17:34:38 +0530

    json parsing error

--- models.go
+++ models.go
@@ -11,7 +11,7 @@ type User struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	Name      string    `json:"string"`
+	Name      string    `json:"name"`
 	ApiKey    string    `json:"api_key"`
 }
 
